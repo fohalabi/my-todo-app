0048ded090890526efd6e2b1f50b41a6a7168384
fohlabi <ayomideabimbola79@gmail.com>
2025-06-01 02:47:40 +0100

    update the filter buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   // This function will be called when a new task is added
@@ -46,6 +54,8 @@ function App(props) {
     setTasks(editedTasks);
   }
 
+  const [filter, setFilter] = useState("All");
+
   const taskList = tasks?.map((task) => (
     <Todo 
      id={task.id} 
@@ -58,6 +68,15 @@ function App(props) {
     />
   ));
 
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name} 
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   const taskNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${tasks.length} tasks remaining`;
 
@@ -66,9 +85,7 @@ function App(props) {
       <h1>My Todo App</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
